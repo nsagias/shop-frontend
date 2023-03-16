28b1a8b4e05255f45612a8068b6ddf371bde1d27
Nick <nsagias@gmail.com>
2023-03-16 19:19:08 -0400

    add redux, react-icons, axios

--- pages/components/container/Container.jsx
+++ pages/components/container/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
   return (
-    <div className="max-w-3xl mx-auto py-8 px-4 space-y-8">
+    <div className="max-w-3xl mx-auto py-0 px-4 space-y-8">
       {children}
     </div>
   );
