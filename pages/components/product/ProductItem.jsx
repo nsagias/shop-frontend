d3ff4da9a086cd2685037f4d8a6abcc06d411696
Nick <nsagias@gmail.com>
2023-03-16 19:04:33 -0400

    add rating component to product item

--- pages/components/product/ProductItem.jsx
+++ pages/components/product/ProductItem.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+import Rating from '@mui/material/Rating';
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 export default function ProductItem({ data }) {
+  const [ratingScore, setRatingScore] = useState(5);
+
   return (
     <div className="bg-white group cursor-pointer space-y-4">
       <img src={data.thumbnail} className="w-60 h-60 border" />
@@ -8,7 +12,10 @@ export default function ProductItem({ data }) {
         <div>
           <h5 className="font-semibold text-lg line-clamp-1">{data.title}</h5>
         <div>
-          <h5>{data.title}</h5>
+        <Rating
+          name="product-rating"
+          value={ratingScore}
+        />
         </div>
         <p className="text-gray-500 text-sm">${data.price}</p>
       </div>
